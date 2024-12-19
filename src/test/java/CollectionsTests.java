952b2f1d85c7237d688866ab0e9d9da08bd2f956
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-12-19 11:23:04 +0200

    ft: handling null values on a hash set

--- src/test/java/CollectionsTests.java
+++ src/test/java/CollectionsTests.java
@@ -2,10 +2,11 @@ import com.dailyDev.collections.HandleNullValues;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CollectionsTests {
     @Test
@@ -19,5 +20,21 @@ public class CollectionsTests {
 
         // accessing null from a list
         assertNull(numbers.getFirst());
+
+        // performing actions on a null values, should return NullPointerException
+        assertThrows(NullPointerException.class,() -> numbers.getFirst().toString());
+    }
+
+    @Test
+    void givenHashSet_whenNullValueAdded_doesNotFail(){
+        Integer[] numberArray = { null, 0, 1, null, 2, 3, null };
+        Set<Integer> numbers = new HashSet<>(Arrays.asList(numberArray));
+        assertEquals(1,HandleNullValues.countNulls.apply(numbers));
+
+        // accessing null values from HashSet
+        assertTrue(numbers.contains(null));
+
+        // performing actions on a null values, should return NullPointerException
+        assertThrows(NullPointerException.class, () -> numbers.forEach(Object::toString));
     }
 }
